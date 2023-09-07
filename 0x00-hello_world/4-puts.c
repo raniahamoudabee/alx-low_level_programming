868b338ae06d3cc14726cef5577d01abc1b0693f
raniahamoudabee <raniahamouda222@gmail.com>
2023-09-07 08:42:57 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-	/**
-	 * woman -Enty point
-	 *
-	 * Return:Always 0 (Success)
-	 * /
-	int main ()
-	{
-		printf("with proper grammar, but the outcome is a piece of art,");
-		return 0;
-	}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
