fc969067d61dc76cd4c440ca4362828840ab2768
raniahamoudabee <raniahamouda222@gmail.com>
2023-09-07 11:33:16 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,8 +9,10 @@
  * Return: Always 0 (Success)
  */
 
-int main(void)
+int main()
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	printf(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	
 	return (1);
+
 }
