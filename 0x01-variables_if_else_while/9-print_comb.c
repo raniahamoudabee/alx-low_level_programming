a9bd3bf083683dcf2ae59b3d96b28b6463bb52cc
raniahamoudabee <raniahamouda222@gmail.com>
2023-09-08 11:17:03 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,21 +10,20 @@
  *
  * Return: Always 0.
  */
-	int main(void)
-	{
-	int num;
+int main(void)
+{
+int num;
 
-	for (num = 0; num <= 9; num++)
-	{
-	putchar((num % 10) + '0');
-	if (num == 9)
-	continue;
-	}
-	putchar(',');
-	putchar(' ');
-	}
+for (num = 0; num <= 9; num++)
+{
+putchar((num % 10) + '0');
+if (num == 9)
+continue;
 
-	putchar('\n');
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
 
-	return (0);
+return (0);
 }
