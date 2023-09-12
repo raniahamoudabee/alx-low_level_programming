d6430ede67e9df45734d41efcaaa9f714ee033d7
raniahamoudabee <raniahamouda222@gmail.com>
2023-09-12 07:57:51 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,13 @@
-#include "putchar.h"
-
-/**
- * main - Print the word "main"
- *
- * Return: Always 0 (Success)
- */
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/*
+*main - Entry point
+*Return: Always 0 (success)
+*program that prints whit put function
+*/
 int main(void)
 {
-	int i;
-	char p[] = "_putchar";
-
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(p[i]);
-	}
-	_putchar('\n');
+	printf("_putchar\n");
 	return (0);
 }
