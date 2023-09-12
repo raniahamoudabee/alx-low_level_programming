d15b6d28c619c146f924bf69005b910a4fc1ef9c
raniahamoudabee <raniahamouda222@gmail.com>
2023-09-12 14:18:01 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,22 @@
 #include "main.h"
+
+
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+* main -Entry point of my code,
+*
+* Return: Always 0
+*/
+
 int main(void)
 {
-printf("_alphabet\n");
-return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
