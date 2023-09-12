341aebfe2d7fdc17438fbbf594666d451e3eb3d4
raniahamoudabee <raniahamouda222@gmail.com>
2023-09-12 07:37:15 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 /*
 *main - Entry point
 *Return: Always 0 (success)
 */
-int main (void)
+int main(void)
 {
 	printf("_putchar\n");
-	return 0 ;
+	
+	return (0);
 }
