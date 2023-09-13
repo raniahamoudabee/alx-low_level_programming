aef179eb054c5b4c43f8517eea19f63c9d6838a0
raniahamoudabee <raniahamouda222@gmail.com>
2023-09-12 21:02:51 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,8 @@
 #include "main.h"
-
-
 /**
 * main -Entry point of my code,
 *
-* Return: Always 0
+* Return: Always (0)
 */
 
 int main(void)
@@ -18,5 +16,5 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-return (0);
+	return (0);
 }
