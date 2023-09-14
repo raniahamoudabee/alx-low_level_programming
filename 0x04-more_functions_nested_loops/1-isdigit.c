34c47e79d7ccc060bf86abca9d3a302ee2a66124
raniahamoudabee <raniahamouda222@gmail.com>
2023-09-14 05:30:36 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,13 +3,13 @@
 
 /**
  * _isdigit - check numbers are 0-9
- *
+ * @c:input for alphabet
  * Return: 0 or 1.
  */
 int _isdigit(int c);
 {
 	if (c >= '0' && c <= '9')
 		return (1);
-	else return (0);
+	else
+		return (0);
 }
-
