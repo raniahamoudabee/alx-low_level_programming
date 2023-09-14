c1c1d78ab368a2cd871f1acc99ce6d8f1ec40e61
raniahamoudabee <raniahamouda222@gmail.com>
2023-09-14 10:29:50 -0700

    my taskes

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
 *print_numbers - function that print all numbers from
 *0 to 9
@@ -8,10 +7,10 @@
 */
 void print_numbers(void)
 {
-	int c;
-for(c = '0';c <= '9'; c++)
+	char c;
+for(c = '0'; c <= '9'; c++)
 	{
-	printf("%d", c);
+	_putchar(c);
 	}
 	_putchar('\n');
 }
