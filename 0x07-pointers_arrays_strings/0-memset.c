669baf8ec4690773c36107028af795213f78e20a
raniahamoudabee <raniahamouda222@gmail.com>
2023-09-25 08:01:32 -0700

    main.h

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /*
- * *_memset - function that fills memory with a constant byte.
+ **_memset - function that fills memory with a constant byte.
  *
  * @b: the address of memory to print
  * @s: the size of the memory to print
@@ -15,6 +15,8 @@ char *_memset(char *s, char b, unsigned int n)
 	int i;
 
 	for (i = 0; i < n; i++)
-	s[i] = b;
+	{
+		s[i] = b;
+	}
 	return (s);
 }
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,4 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
+
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
