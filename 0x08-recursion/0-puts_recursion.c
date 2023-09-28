5bd4b2b92c2de10055e47dfff0df4e99d433cd2c
raniahamoudabee <raniahamouda222@gmail.com>
2023-09-28 01:00:26 -0700

    a

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - check the code
+ *
+ *@puts: input
+
+ * Return: man puts.
+ */
+
+void _puts_recursion(char *s);
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_ptuchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
