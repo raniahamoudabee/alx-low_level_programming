250f0e34d235add16d135d42e8cb3e5ce02b53ad
raniahamoudabee <raniahamouda222@gmail.com>
2023-10-02 07:01:00 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main: peint the numbers
+ * @aegc: number
+ * @argv: array
+ *
+ * RETURN:(0)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc -1);
+	return (0);
+}
