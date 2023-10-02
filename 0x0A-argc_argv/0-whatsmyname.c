95f0242efdd04f05c0b253ce5d108b5e6fc38c02
raniahamoudabee <raniahamouda222@gmail.com>
2023-10-02 06:22:51 -0700

    q

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * name - program that prints its name, followed by a new line
+ * @aegc:input
+ * @aegv:output
+ *
+ * RETURN: (0)
+ */
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+		return (0);
+}
